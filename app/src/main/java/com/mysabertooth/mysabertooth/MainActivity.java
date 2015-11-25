08de80d2af3cc2756102f81ce10a2427cd3acfd8
Cassie <cassieleighwang@gmail.com>
2015-11-25 14:34:04 +0800

    Added help dialog

--- app/src/main/java/com/mysabertooth/mysabertooth/MainActivity.java
+++ app/src/main/java/com/mysabertooth/mysabertooth/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.oralb.sdk.OBTBrush;
 import com.oralb.sdk.OBTSDK;
@@ -16,6 +18,10 @@ import com.oralb.sdk.OBTSDK;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button fishButton;
+    public Button mainHelpDialogOk;
+    public LinearLayout mainHelpDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,25 @@ public class MainActivity extends AppCompatActivity {
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
+
+        mainHelpDialog = (LinearLayout) findViewById(R.id.help_dialog);
+        mainHelpDialogOk = (Button) findViewById(R.id.btn_fish_dialog_ok);
+        fishButton = (Button) findViewById(R.id.btn_fish);
+
+        mainHelpDialog.setVisibility(View.GONE);
+        fishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainHelpDialog.setVisibility(View.VISIBLE);
+            }
+        });
+
+        mainHelpDialogOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainHelpDialog.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
